94c0eecac23fbb0e41a0142afdcf95ec0301e65d
Bibhor <bibhorsharma7@gmail.com>
2023-04-11 21:35:45 -0400

    add chart.js

--- web-client/components/navbar/nav.tsx
+++ web-client/components/navbar/nav.tsx
@@ -4,12 +4,13 @@ const Nav = () => {
   return (
     <div className="flex h-24 min-w-full flex-row items-center bg-sky-300 p-2">
       {/* <Image src="/logo.png" alt="SynECG" width={250} height={250} /> */}
-      <div className="">
+      <Link href="/" className="">
         <img src="/logo.png" alt="SynECG" className="w-30 h-20 rounded-md" />
-      </div>
-      <div className="flex flex-1 flex-row items-center justify-center space-x-8">
+      </Link>
+      <div className="flex flex-1 flex-row items-center space-x-8 pl-20">
         <Link href="/">Home</Link>
         <Link href="/about">About</Link>
+        <Link href="/signal">Signal</Link>
       </div>
     </div>
   );
---
+++ web-client/pages/signal.tsx
@@ -0,0 +1,12 @@
+import LineChart from "@/components/chart";
+
+const Signal = () => {
+  return (
+    <div>
+      <h1 className="text-xl">Graph</h1>
+      <LineChart />
+    </div>
+  );
+};
+
+export default Signal;
