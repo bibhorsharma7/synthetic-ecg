7e165f8ab1ba5eecf8e6177a9bba524f8e4f6b6b
Bibhor Sharma <bibhorsharma7@gmail.com>
2024-04-02 20:29:40 +0545

    feat(server): add aws sam template for lambda functions

---
+++ lambda/src/app.py
@@ -0,0 +1,15 @@
+import json
+
+# import requests
+
+
+def handler(event, context):
+    return {
+        "statusCode": 200,
+        "body": json.dumps(
+            {
+                "message": "hello world",
+                # "location": ip.text.replace("\n", "")
+            }
+        ),
+    }
