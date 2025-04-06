e2ebe2c28e289c08fd5248ff3303a07092ded517
Bibhor Sharma <bibhorsharma7@gmail.com>
2025-04-06 20:49:21 +1000

    fix: remove client components

--- web-client/components/navbar.tsx
+++ web-client/components/navbar.tsx
@@ -1,46 +1,20 @@
-"use client";
-
 import Link from "next/link";
 import Image from "next/image";
-import { UserRound } from "lucide-react";
-import {
-  DropdownMenu,
-  DropdownMenuTrigger,
-  DropdownMenuItem,
-  DropdownMenuContent,
-} from "@radix-ui/react-dropdown-menu";
 
 export default function Navbar() {
   return (
-    <div className="flex h-24 w-full flex-row justify-between space-x-4 bg-sky-300 p-2">
-      <div>
-        <Link href="/">
-          <Image
-            src="/images/logo.png"
-            height={1}
-            width={100}
-            alt="SynECG"
-            className="rounded-md"
-          />
-        </Link>
-      </div>
-      <div className="flex flex-row items-center">
-        <h1 className="">SynECG App</h1>
-      </div>
-      <div className="flex flex-row items-center">
-        <DropdownMenu>
-          <DropdownMenuTrigger>
-            <UserRound />
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuItem>
-              <Link href="/login">Login</Link>
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              <Link href="/signup">SignUp</Link>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+    <div className="flex h-24 w-full flex-row space-x-4 bg-sky-300 p-2">
+      <Link href="/">
+        <Image
+          src="/images/logo.png"
+          height={1}
+          width={100}
+          alt="SynECG"
+          className="rounded-md"
+        />
+      </Link>
+      <div className="flex w-full flex-1 items-center justify-center">
+        <h1 className="">Synthetic ECG Generator</h1>
       </div>
     </div>
   );
