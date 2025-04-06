d351a91e732b92ea8b004d93440780709c13741d
Bibhor Sharma <bibhorsharma7@gmail.com>
2025-04-06 19:27:43 +1000

    add: fast api backend initial

---
+++ server/main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+@app.get("/")
+async def main():
+    return {"message": "Synthetic ECG Generator API"}
+
+
+class Parameters(BaseModel):
+    rate1: int
+    rate2: int | None
+    time: int
+    a1: float
+    a2: float
+    a3: float
+    a4: float
+    a5: float
+    b1: float
+    b2: float
+    b3: float
+    b4: float
+    b5: float
+    th1: float
+    th2: float
+    th3: float
+    th4: float
+    th5: float
+    z0: float
+    x: float 
+    y: float 
+    z: float 
+
+
+
+@app.post("/solve")
+async def solve(parameters: Parameters):
+    print('parameters:', parameters)
+    res, finalValues = ode.solve(parameters)
+    return {"graph": res, "finalValues": finalValues}
