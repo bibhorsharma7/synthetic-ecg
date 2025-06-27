5e072957217eb86be8215205ca90cd95521b18b4
Bibhor Sharma <bibhorsharma7@gmail.com>
2025-06-27 14:56:03 +1000

    wip: stash

--- web-client/app/components/graph.tsx
+++ web-client/app/components/graph.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { DEFAULT_ECG } from "@/lib/constants";
 import { Line, CartesianGrid, LineChart, XAxis, YAxis } from "recharts";
 
 type PointType = {
@@ -9,15 +10,16 @@ type PointType = {
 export default function Graph({ data }: { data: PointType[] }) {
   return (
     <div>
-      <LineChart data={data} width={500} height={300}>
+      <LineChart width={800} height={300}>
         <CartesianGrid strokeDasharray="1 1" />
-        <XAxis dataKey="time" />
-        <YAxis dataKey="value" />
+        <XAxis dataKey="time" type="number" unit="s" />
+        <YAxis dataKey="value" type="number" unit="mV" />
         <Line
           name="Modeled"
-          data={data}
+          data={DEFAULT_ECG}
           dataKey="value"
           dot={false}
+          type="monotone"
           stroke="#8884d8"
         />
       </LineChart>
